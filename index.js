c75b7d2f1eceae738457f0fb6b4c27f5a2a93b0e
Nivth <kevinzevalja@gmail.com>
2022-08-23 10:13:11 +0700

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+// tipe data
+var string = "string";
+var integer = 12;
+var float = 1.4;
+var boolean = true || false;
+var object = {
+  woi: "anjim",
+  int: 12,
+  kafir: true,
+};
+var array = ["babank", "dewa", "asdasdsa", "asdasdsa"];
+
+console.log(array[1]);// array
+console.log(object.int)
