e8c2aec12aa408632d892f97e10e498e1e6db77a
Nivth <kevinzevalja@gmail.com>
2022-09-03 06:09:31 +0700

    update chapter 3

--- chapter-03/08-composition/04-imperative-clock.js
+++ chapter-03/08-composition/04-imperative-clock.js
@@ -2,55 +2,49 @@
 setInterval(logClockTime, 1000);
 
 function logClockTime() {
+  // Dapatkan string Waktu sebagai waktu sipil
+  var time = getClockTime();
 
-    // Get Time string as civilian time
-    var time = getClockTime();
-
-    // Clear the Console and log the time
-    console.clear();
-    console.log(time);
+  // Kosongkan Console dan catat waktunya
+  console.clear();
+  console.log(time);
 }
 
 function getClockTime() {
+  // Dapatkan Waktu Saat Ini
+  var date = new Date();
 
-    // Get the Current Time
-    var date = new Date();
+  // Serialisasi waktu jam
+  var time = {
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+    ampm: "AM",
+  };
 
-    // Serialize clock time
-    var time = {
-        hours: date.getHours(),
-        minutes: date.getMinutes(),
-        seconds: date.getSeconds(),
-        ampm: "AM"
-    }
+  // Ubah ke waktu sipil
+  if (time.hours == 12) {
+    time.ampm = "PM";
+  } else if (time.hours > 12) {
+    time.ampm = "PM";
+    time.hours -= 12;
+  }
 
-    // Convert to civilian time
-    if (time.hours == 12) {
-        time.ampm = "PM";
-    } else if (time.hours > 12) {
-        time.ampm = "PM";
-        time.hours -= 12;
-    }
+  // Tambahkan 0 pada jam untuk membuat dua digit
+  if (time.hours < 10) {
+    time.hours = "0" + time.hours;
+  }
 
-    // Prepend a 0 on the hours to make double digits
-    if (time.hours < 10) {
-        time.hours = "0" + time.hours;
-    }
+  // tambahkan 0 pada menit untuk membuat dua digit
+  if (time.minutes < 10) {
+    time.minutes = "0" + time.minutes;
+  }
 
-    // prepend a 0 on the minutes to make double digits
-    if (time.minutes < 10) {
-        time.minutes = "0" + time.minutes;
-    }
+  // tambahkan 0 pada detik untuk membuat dua digit
+  if (time.seconds < 10) {
+    time.seconds = "0" + time.seconds;
+  }
 
-    // prepend a 0 on the seconds to make double digits
-    if (time.seconds < 10) {
-        time.seconds = "0" + time.seconds;
-    }
-
-    // Format the clock time as a string "hh:mm:ss tt"
-    return time.hours + ":"
-        + time.minutes + ":"
-        + time.seconds + " "
-        + time.ampm;
-
+  // Format waktu jam sebagai string "hh:mm:ss tt"
+  return time.hours + ":" + time.minutes + ":" + time.seconds + " " + time.ampm;
 }
