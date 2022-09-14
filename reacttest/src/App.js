1d2afebcaea96e6bee4de6acf526637c2b1c9ddd
Nivth <kevinzevalja@gmail.com>
2022-09-14 09:24:38 +0700

    Add React Test && Add Chapter 7

---
+++ reacttest/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./page/Home";
+import About from "./page/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ reacttest/src/page/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="blue">
+      Home
+      <h1>Jokowi</h1>
+    </div>
+  );
+}
